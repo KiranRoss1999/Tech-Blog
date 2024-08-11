72c669f6f893f9cc9a7ef830b9f8fec1ad316938
KiranRoss <120018062+KiranRoss@users.noreply.github.com>
2024-08-11 23:15:02 +1000

    render updates

--- config/connection.js
+++ config/connection.js
@@ -7,10 +7,9 @@ const sequelize = process.env.DB_URL
       process.env.DB_NAME,
       process.env.DB_USER,
       process.env.DB_PASSWORD,
-      process.env.DB_HOST,
-      process.env.DB_PORT,
       {
-        host: 'localhost',
+        host: process.env.DB_HOST || 'localhost',
+        port: process.env.DB_PORT || 5432,
         dialect: 'postgres',
       }
     );
